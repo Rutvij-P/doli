e5bd20ce94caad34d0ad16b02edb98719a843d16
Rutu Patel <patelrutvij2801@gmail.com>
2023-06-11 13:50:00 -0700

    car list card updates

--- car-recommendation/src/client/pages/CarList.tsx
+++ car-recommendation/src/client/pages/CarList.tsx
@@ -27,20 +27,22 @@ const CarList = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Car List</h1>
-      <ul>
-        {cars.map((car) => (
-          <li key={car.name}>
-            <img src={car.picture} alt={car.name} />
-            <h2>{car.name}</h2>
-            <p>Company: {car.company}</p>
-            <p>Drive Type: {car.driveType}</p>
-            <p>Vehicle Type: {car.vehicleType}</p>
-            <p>Seats: {car.seats}</p>
-          </li>
-        ))}
-      </ul>
+    <div className="flex-col pt-100px">
+        <h3 className="text-3vw flex justify-evenly">Car List</h3>
+        <ul className='flex flex-wrap items-stretch py-4 justify-evenly'>
+          {cars.map((car) => (
+            <div className='flex-0 m-2.5 rounded-2xl border-gray-300 shadow-md h-108 w-96 object-fit' key={car.name}>
+              <li className='' key={car.name}>
+                <img className='rounded-2xl' src={car.picture} alt={car.name} />
+                <h2>{car.name}</h2>
+                <p>Company: {car.company}</p>
+                <p>Drive Type: {car.driveType}</p>
+                <p>Vehicle Type: {car.vehicleType}</p>
+                <p>Seats: {car.seats}</p>
+              </li>
+            </div>
+          ))}
+        </ul>     
     </div>
   );
 };
